29602f804dd54a17ef551a0cfdb82228d0636688
Vaibhav <vaibhavsharma.cs@gmail.com>
2021-04-14 09:45:28 +0530

    Work pending

---
+++ JWTAsmt/Utilities/JsonWebToken.swift
@@ -0,0 +1,76 @@
+//
+//  JsonWebToken.swift
+//  JWTAsmt
+//
+//  Created by Vaibhav Sharma on 10/04/21.
+//
+
+import CryptoKit
+import UIKit
+
+class JsonWebToken {
+    
+    func getJWT() -> String{
+        return self.generateJwtToken()
+    }
+    
+    private func generateJwtToken() -> String{
+        
+        let secret = "58627c2e-d2f7-47ec-8ab6-b193a1f88658"
+        let privateKey = SymmetricKey(data: secret.data(using: .utf8)!)
+
+        let headerJSONData = try! JSONEncoder().encode(Header())
+        let headerBase64String = headerJSONData.urlSafeBase64EncodedString()
+
+        let payloadJSONData = try! JSONEncoder().encode(Payload())
+        print("####", payloadJSONData.description)
+        let payloadBase64String = payloadJSONData.urlSafeBase64EncodedString()
+
+        let toSign = (headerBase64String + "." + payloadBase64String).data(using: .utf8)!
+
+        let signature = HMAC<SHA256>.authenticationCode(for: toSign, using: privateKey)
+        print("signature: ", signature)
+        let signatureBase64String = Data(signature).urlSafeBase64EncodedString()
+
+        let token = [headerBase64String, payloadBase64String, signatureBase64String].joined(separator: ".")
+        return token // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+    }
+}
+
+extension Data {
+    func urlSafeBase64EncodedString() -> String {
+        return base64EncodedString()
+            .replacingOccurrences(of: "+", with: "-")
+            .replacingOccurrences(of: "/", with: "_")
+            .replacingOccurrences(of: "=", with: "")
+    }
+}
+
+struct Header: Encodable {
+    let alg = "HS256"
+    let typ = "JWT"
+}
+
+struct Payload: Encodable {
+    
+    /**
+     {
+     "clientId": "ebc38746-4bbe-4fd9-b695-3bd44ecb3afc", "individualId": "591cbcdf-4fec-4206-b651-2b7bf645979e",
+     "canAssess": [ "591cbcdf-4fec-4206-b651-2b7bf645979e"
+     ], "canMonitor": [
+     "591cbcdf-4fec-4206-b651-2b7bf645979e" ],
+     "role": "consumer",
+     "exp": "1497281798"
+     }
+     */
+    let clientId = "ebc38746-4bbe-4fd9-b695-3bd44ecb3afc"
+    let individualId = "591cbcdf-4fec-4206-b651-2b7bf645979e"
+    let canAssess = ["591cbcdf-4fec-4206-b651-2b7bf645979e"]
+    let canMonitor = ["591cbcdf-4fec-4206-b651-2b7bf645979e"]
+    let role = "consumer"
+    let exp = (Date() + 300 /*in seconds*/).timeIntervalSince1970
+}
+
+
+
+
--- JWTAsmt/ViewController.swift
+++ JWTAsmt/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import CryptoKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "JWT Token"
+        print((Date() + 300).timeIntervalSince1970)
+        
+        let obj = JsonWebToken()
+        print(obj.getJWT())
         // Do any additional setup after loading the view.
     }
-
 
 }
 
